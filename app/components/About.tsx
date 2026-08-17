40840cf1efc687ae6443d33a63e7cc151ff9293e
shezafatima <sohailejaz058@gmail.com>
2026-08-17 21:13:58 +0500

    features added

--- app/components/About.tsx
+++ app/components/About.tsx
@@ -6,7 +6,7 @@ const About = () => {
     <div id="about">
       <MotionWrapper>
         <section className="text-gray-600 body-font">
-          <div className="container mx-auto flex px-5 pt-20 pb-12 md:flex-row flex-col items-center">
+          <div className="container mx-auto flex px-5 pt-20 pb-12 lg:flex-row flex-col items-center">
             <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
               <h1 className="title-font sm:text-5xl sm:font-bold text-3xl mb-4  drop-shadow-lg text-white">
                 About me
@@ -30,8 +30,8 @@ const About = () => {
             </div>
             <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
             <Image
-              className="object-cover object-center rounded sm:w-[450px]"
-              src="/images/about.png"
+              className="object-cover object-center rounded-2xl sm:w-[450px]"
+              src="/images/aboutme.png"
               alt="about"
               width={500}
               height={500}
